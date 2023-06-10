6aa804b4f7d6186ac965ad7d1f30fea54e00be44
Abhiraj Dighe <digheabhirajr262@gmail.com>
2023-06-10 14:32:04 +0530

    Linking and Navigation

---
+++ src/app/login/page.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const Login =()=>{
+    return(
+        <div>
+            <h1>Login in page</h1>
+            <Link href="/">Got to Home page</Link>
+        </div>
+    )
+}
+
+export default Login;
--- src/app/page.js
+++ src/app/page.js
@@ -2,18 +2,26 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import { useState } from 'react'
+import Link from 'next/link'
+import {useRouter} from 'next/navigation'
+ 
 
 export default function Home() {
-  const [name,setName]=useState("Abhi");
-  const fruit =()=>{
-    // alert(item)
-    setName("Abhiraj Dighe")
-  }
+  // const [name,setName]=useState("Abhi");
+  // const fruit =()=>{
+  //   // alert(item)
+  //   setName("Abhiraj Dighe")
+  // }
 
-  const InnerComp=()=>{
-    return(
-      <h1>Inner Component</h1>
-    )
+  // const InnerComp=()=>{
+  //   return(
+  //     <h1>Inner Component</h1>
+  //   )
+  // }
+
+  const router = useRouter();
+  const navigate =(name)=>{
+    router.push(name)
   }
   return (
     // <main className={styles.main}>
@@ -22,24 +30,43 @@ export default function Home() {
     //   <h1>Home Page</h1>
     // </main>
 
-    <main className={styles.main}>
-      <h1>Event and function in Next js ,Welcome {name}</h1>
-      {/* <button onClick={()=>fruit("apple")}>click me</button> */}
-      <InnerComp/>
+        
+    // <main className={styles.main}>
+    //   <h1>Event and function in Next js ,Welcome {name}</h1>
+    //   {/* <button onClick={()=>fruit("apple")}>click me</button> */}
+    //   <InnerComp/>
       
-      {/* {InnerComp()} */}
+    //   {/* {InnerComp()} */}
 
-      <button onClick={()=>fruit()}>click me</button>
+    //   <button onClick={()=>fruit()}>click me</button>
 
+    // </main>
+
+
+
+        // Linking and Navigation
+
+    <main>
+      <h1>Basic routing | Make new Pages</h1>
+      <Link href="/login">Go to Login Page</Link>
+      <br/>
+      <Link href="/about">Go to about Page</Link>
+
+      <br/>
+      <br/>
+
+      <button onClick={()=>router.push("/login")}>Go to Login Page</button>
+      <br/>
+      <button onClick={()=>navigate("/about")}>Go to About Page</button>
     </main>
   )
 }
 
-const User=(props)=>{
-  return(
-    <div>
-      <h2>User name in {props.name}</h2>
+// const User=(props)=>{
+//   return(
+//     <div>
+//       <h2>User name in {props.name}</h2>
 
-    </div>
-  )
-}
+//     </div>
+//   )
+// }
